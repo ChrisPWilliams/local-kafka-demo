d470e668cfb8e086c8afdb3fb0677d4a58ab5fed
Chris Williams <chris.p.williams.314@gmail.com>
2024-01-05 16:52:01 +0000

    Add comments

--- consumer.py
+++ consumer.py
@@ -1,17 +1,15 @@
 from kafka import KafkaConsumer
 
 consumer = KafkaConsumer(
-    bootstrap_servers=["localhost:29092"],
-    auto_offset_reset="earliest",
-    group_id="login_consumer",
-    consumer_timeout_ms=1000
+    bootstrap_servers=["localhost:29092"],  # Set cluster address
+    auto_offset_reset="earliest",           # If there is an offset error, go to the earliest uncommitted offset
+    group_id="login_consumer",              # Set a consumer group name in order for the Kafka cluster to track the last consumed offset
+    consumer_timeout_ms=1000                # Shut down the consumer if there are no unconsumed messages in the topic for more than one second
     )
-consumer.subscribe(["data_engineers", "software_engineers"])
+consumer.subscribe(["data_engineers", "software_engineers"])  # Subscribe to our topics
 
-for message in consumer:
-    print(message.value)
+for message in consumer:                    # Note that "consumer" is an Iterator that will stay open until we shut down the consumer
+    print(message.value)                    # Print all message values for each message in the topic. 
 
-"""consumer is an Iterator object that returns messages from the topic, it doesn't know when the topic is 
-no longer going to receive messages, so we need to end the process manually with keyboard interrupt (CTRL-C)"""
-consumer.commit()
-consumer.close()
+consumer.commit()                           # Before shutting down the consumer, ensure offsets are committed to the cluster, so that the
+consumer.close()                            # consumer can continue from where it left off if new messages are produced
--- producer.py
+++ producer.py
@@ -1,15 +1,19 @@
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers="localhost:29092")
+producer = KafkaProducer(bootstrap_servers="localhost:29092")   # "bootstrap_servers" is the address of our Kafka cluster nodes
+
+# Suppose we want to track logins of software & data engineers to a system. First we generate messages to appropriate topics to track logins
+# 10 logins of each kind
 
 for i in range(10):
     print(f"sending {i}")
-    message_bytes = f"{i} logged in".encode('utf-8')
-    producer.send(topic="data_engineers", value=message_bytes)
+    message_bytes = f"{i} logged in".encode('utf-8')            # need to encode message string as binary
+    producer.send(topic="data_engineers", value=message_bytes)  # send message to data_engineers topic
 for i in range(10):
     print(f"sending {i}")
     message_bytes = f"{i} logged in".encode('utf-8')
     producer.send(topic="software_engineers", value=message_bytes)
 
-producer.flush()
+# Kafka bundles messages into packets for improved network throughput, so it does not always send messages immediately 
+producer.flush()            # Call producer.flush to ensure all messages are sent before ending the script                                                
 producer.close()
